9d30abe271428db2a12a3cd31bb5d94395a3dde7
shivamkulkarni <shivamkulkarnimu10@gmail.com>
2019-11-25 14:41:41 -0600

    Update DataProcessing.R

--- DataProcessing.R
+++ DataProcessing.R
@@ -1,30 +1,41 @@
+#Read cleaned data
 new.chicago.df <- read.csv("clean_data.csv", header = T, stringsAsFactors = F)
+
+#Splits the the dates as date and time 
 time1 <- strsplit(new.chicago.df$Date, " ")
+
+#Extracts the time
 times <- c()
 for(n in 1:nrow(new.chicago.df))
 {
   times <- c(times, as.character(time1[[n]][2]))
 }
 
+#Splits the time as HH MM 
 split <- strsplit(times, ":")
+
+#Extracts the hour
 hour <- c()
 for(n in 1:nrow(new.chicago.df))
 {
   hour <- c(hour,as.numeric(split[[n]][1]))
 }
-
+#Extracts either AM or PM
 time2 <- c()
 for(n in 1:nrow(new.chicago.df))
 {
   time2 <- c(time2, as.character(time1[[n]][3]))
 }
 
+#Converts time in 24 hour format
 condition <- time2 == "PM"
 hour[condition] <- hour[condition] + 12
 
 hour.of.the.day <- hour
 
+#Adds the hour column to the data
 new.chicago.df <- cbind(new.chicago.df,hour.of.the.day)
+
 
 dates <- c()
 for(n in 1:nrow(new.chicago.df))
@@ -41,6 +52,7 @@ for(n in 1:nrow(new.chicago.df))
 
 season <- c(rep("Winter", nrow(new.chicago.df)))
 
+#Assigns the correct season according to the month
 for(n in 1:nrow(new.chicago.df))
 {
   if(month[n] >= 3 && month[n] < 6)
